91e90d88fb3b915ba4fdefb963b27f86a318c73e
vera-codes6 <vera.codes6@gmail.com>
2026-04-29 10:03:29 -0400

    update shell sorter

--- Algorithms/Sorters/Comparison/ShellSorter.cs
+++ Algorithms/Sorters/Comparison/ShellSorter.cs
@@ -18,9 +18,9 @@ public class ShellSorter<T> : IComparisonSorter<T>
     /// <param name="comparer">Compares elements.</param>
     public void Sort(T[] array, IComparer<T> comparer)
     {
-        for (var step = array.Length / 2; step > 0; step /= 2)
+        for (int step = array.Length / 2; step > 0; step /= 2)
         {
-            for (var i = 0; i < step; i++)
+            for (int i = 0; i < step; i++)
             {
                 GappedBubbleSort(array, comparer, i, step);
             }
@@ -29,14 +29,14 @@ public class ShellSorter<T> : IComparisonSorter<T>
 
     private static void GappedBubbleSort(T[] array, IComparer<T> comparer, int start, int step)
     {
-        for (var j = start; j < array.Length - step; j += step)
+        for (int j = start; j < array.Length - step; j += step)
         {
-            var wasChanged = false;
-            for (var k = start; k < array.Length - j - step; k += step)
+            bool wasChanged = false;
+            for (int k = start; k < array.Length - j - step; k += step)
             {
                 if (comparer.Compare(array[k], array[k + step]) > 0)
                 {
-                    var temp = array[k];
+                    T temp = array[k];
                     array[k] = array[k + step];
                     array[k + step] = temp;
 
@@ -44,7 +44,7 @@ public class ShellSorter<T> : IComparisonSorter<T>
                 }
             }
 
-            if (!wasChanged)
+            if (wasChanged != false)
             {
                 break;
             }
